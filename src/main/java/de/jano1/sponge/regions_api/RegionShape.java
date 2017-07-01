25afe7bd5cf7963c830951b0c87db781efba776f
Jan Leißner <jleissner@uos.de>
2017-07-01 19:12:02 +0200

    Added some description 3

--- src/main/java/de/jano1/sponge/regions_api/RegionShape.java
+++ src/main/java/de/jano1/sponge/regions_api/RegionShape.java
@@ -6,5 +6,11 @@ import org.spongepowered.api.world.Location;
  * Created by Jano1 on 01.07.2017.
  */
 public interface RegionShape {
+
+    /**
+     * Test if this Shape contains a Location
+     * @param location The Location to test
+     * @return True if this shape contains the location, false if not
+     */
     public boolean contains(Location location);
 }
