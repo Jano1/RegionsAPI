65811d217be2dce9137df17f04404b78a81d5062
Jan Leißner <jleissner@uos.de>
2017-07-01 16:21:02 +0200

    Some further developement stuff

--- src/main/java/de/jano1/sponge/regions_api/RegionService.java
+++ src/main/java/de/jano1/sponge/regions_api/RegionService.java
@@ -1,6 +1,7 @@
 package de.jano1.sponge.regions_api;
 
 import org.spongepowered.api.service.context.ContextualService;
+import org.spongepowered.api.world.Location;
 
 /**
  * Created by jleissner on 01.07.2017.
@@ -10,4 +11,6 @@ public interface RegionService extends ContextualService<Region>{
     public Region getRegionWithID(String region_id);
     public void addRegion(Region region);
     public void removeRegion(String region_id);
+
+    public Region[] getRegionsThatContains(Location location);
 }
--- src/main/java/de/jano1/sponge/regions_api/RegionShape.java
+++ src/main/java/de/jano1/sponge/regions_api/RegionShape.java
@@ -1,7 +1,10 @@
 package de.jano1.sponge.regions_api;
 
+import org.spongepowered.api.world.Location;
+
 /**
  * Created by jleissner on 01.07.2017.
  */
 public interface RegionShape {
+    public boolean inShape(Location location);
 }
