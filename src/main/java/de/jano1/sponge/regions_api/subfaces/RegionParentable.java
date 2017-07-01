a9737982f420e0d8323954387fccc58ba877eb94
Jan Leißner <jleissner@uos.de>
2017-07-01 19:08:22 +0200

    Added some description 2

--- src/main/java/de/jano1/sponge/regions_api/subfaces/RegionParentable.java
+++ src/main/java/de/jano1/sponge/regions_api/subfaces/RegionParentable.java
@@ -4,8 +4,36 @@ package de.jano1.sponge.regions_api.subfaces;
  * Created by Jano1 on 01.07.2017.
  */
 public interface RegionParentable {
+
+    /**
+     * Get all the ids of the parents of this region
+     * @return Array of parent-ids for this region
+     */
     public String[] getParents();
-    public boolean isParent(String plugin_id);
-    public void setParentalStatus(String plugin_id, boolean status);
-    public void addParent(String plugin_id);
+
+    /**
+     * Test if this region has parents at all
+     * @return True if parents exist, false if not
+     */
+    public boolean hasParents();
+
+    /**
+     * Test if this region has a specific parent
+     * @param region_id The region id to test
+     * @return True if given region is parent of this region
+     */
+    public boolean hasParent(String region_id);
+
+    /**
+     * Set a parent for this region (should also unset if status equals false)
+     * @param region_id The desired parent
+     * @param status The desired setting
+     */
+    public void setParentalStatus(String region_id, boolean status);
+
+    /**
+     * Shorter version of @see{setParentalStatus()}, use this to add a parent in a shorter way
+     * @param region_id The desired parent
+     */
+    public void addParent(String region_id);
 }
