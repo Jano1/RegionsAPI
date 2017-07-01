78e241c0fa21c63aabfe85c7f2f56e04da041b3b
Jan Leißner <jleissner@uos.de>
2017-07-01 16:11:55 +0200

    Made Region contextual and RegionService a ContextualService

--- src/main/java/de/jano1/sponge/regions_api/Region.java
+++ src/main/java/de/jano1/sponge/regions_api/Region.java
@@ -2,12 +2,12 @@ package de.jano1.sponge.regions_api;
 
 import de.jano1.sponge.regions_api.subfaces.PluginRelatable;
 import de.jano1.sponge.regions_api.subfaces.RegionParentable;
+import org.spongepowered.api.service.context.Contextual;
 
 /**
  * Created by jleissner on 01.07.2017.
  */
-public interface Region extends PluginRelatable, RegionParentable {
+public interface Region extends PluginRelatable, RegionParentable, Contextual {
     public Shape getShape();
-    public String getRegionID();
     public Flag[] getFlags();
 }
--- src/main/java/de/jano1/sponge/regions_api/RegionService.java
+++ src/main/java/de/jano1/sponge/regions_api/RegionService.java
@@ -1,9 +1,11 @@
 package de.jano1.sponge.regions_api;
 
+import org.spongepowered.api.service.context.ContextualService;
+
 /**
  * Created by jleissner on 01.07.2017.
  */
-public interface RegionService {
+public interface RegionService extends ContextualService<Region>{
     public Region[] getRegions();
     public Region getRegionWithID(String region_id);
     public void addRegion(Region region);
