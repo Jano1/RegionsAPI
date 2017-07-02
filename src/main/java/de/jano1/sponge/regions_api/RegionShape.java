dc174c527db2e508804db27687bd6820fc459baa
Jan Leißner <jleissner@uos.de>
2017-07-02 16:27:49 +0200

    Vertices are addable and removeable for Shapes

--- src/main/java/de/jano1/sponge/regions_api/RegionShape.java
+++ src/main/java/de/jano1/sponge/regions_api/RegionShape.java
@@ -1,6 +1,7 @@
 package de.jano1.sponge.regions_api;
 
 import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
 /**
  * Created by Jano1 on 01.07.2017.
@@ -13,4 +14,16 @@ public interface RegionShape {
      * @return True if this shape contains the location, false if not
      */
     public boolean contains(Location location);
+
+    /**
+     * Add a vertice to the Shape
+     * @param location
+     */
+    public void addVertice(Location<World> location);
+
+    /**
+     * Removes a vertice in the shape
+     * @param location
+     */
+    public void removeVertice(Location<World> location);
 }
